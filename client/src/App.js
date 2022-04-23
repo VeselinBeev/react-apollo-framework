82a394895a893493cebfac09d5e0d6aae11f36a1
VeselinBeev <veselinbeev@gmail.com>
2022-04-23 14:06:16 +0300

    dd1

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+import { ThemeContext } from "styled-components";
+import { useContext } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const Button = styled.button`
+        background: transparent;
+        border-radius: 3px;
+        border: 2px solid palevioletred;
+        color: palevioletred;
+        margin: 0.5em 1em;
+        padding: 0.25em 1em;
+    `;
+
+    const Container = styled.div`
+        text-align: center;
+    `;
+    const themeContext = useContext(ThemeContext);
+
+    return (
+        <div className="Layout">
+            <header className="header">
+                <Container>
+                    <Button primary>Primary Button</Button>
+                </Container>
+            </header>
+        </div>
+    );
 }
 
 export default App;
